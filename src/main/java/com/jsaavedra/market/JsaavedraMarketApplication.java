e487d6853703f2f8d1300711b2fbea4d7b5bbdb0
jrsaavedra1022 <robertosaavedra1022@gmail.com>
2022-04-30 12:06:36 -0500

    init

---
+++ src/main/java/com/jsaavedra/market/JsaavedraMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jsaavedra.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsaavedraMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsaavedraMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsaavedra/market/JsaavedraMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsaavedra.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsaavedraMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
